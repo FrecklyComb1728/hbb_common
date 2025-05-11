0e6469da97fb179dbad454d99953b5b4b820e60d
蜜蜂QWQ <158593086+FrecklyComb1728@users.noreply.github.com>
2025-05-11 19:17:23 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,13 +98,13 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk-server.bee-zh.cn"];
+pub const RS_PUB_KEY: &str = "";
 
-pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const RELAY_PORT: i32 = 21117;
-pub const WS_RENDEZVOUS_PORT: i32 = 21118;
-pub const WS_RELAY_PORT: i32 = 21119;
+pub const RENDEZVOUS_PORT: i32 = 15116;
+pub const RELAY_PORT: i32 = 15117;
+pub const WS_RENDEZVOUS_PORT: i32 = 15118;
+pub const WS_RELAY_PORT: i32 = 15119;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
